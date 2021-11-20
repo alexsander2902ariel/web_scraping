4de97fa013ad0d36684b9853ac96e61fb004a939
alexsander2902ariel <alexsander2902ariel@gmail.com>
2021-11-20 18:57:18 -0300

    web scraping and schedule

---
+++ main.py
@@ -0,0 +1,31 @@
+""" main.py """
+
+# Libraries
+import requests
+from bs4 import BeautifulSoup as bf
+import schedule
+import time
+
+# Parameters
+from parameters import url, headers, interval
+
+def search_job():
+    response = requests.get(url= url,headers= headers)
+    html = bf(response.text, 'html.parser')
+
+    for item in html.select('.project-item'):
+        
+        time = item.select_one('.project-header strong').text
+        title = item.select_one('.project-title span').text
+        price = item.select_one('.budget span').text
+        abstract = item.select_one('.project-details').text
+        if ('minuto' in time) or ('Agora' in time):
+            print(f'\n\n{time}\n{title} => {price}{abstract}\n', end=("_"*130))
+    
+schedule.every(interval).minutes.do(search_job)
+
+if __name__ == '__main__':
+    search_job()
+    while 1:
+      schedule.run_pending()
+      time.sleep(1)
---
+++ parameters.py
@@ -0,0 +1,6 @@
+""" Parameters.py """
+
+# url, user-agente and interval scheduler
+url = 'https://www.workana.com/jobs?language=pt&publication=1d&skills=python'
+headers = {'user-agent': 'Chrome/96.0.4664.45'}
+interval = 5
